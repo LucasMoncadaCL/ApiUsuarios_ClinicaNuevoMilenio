ab5040ab6398006b0b00c785b85106d101c4a39d
LucasMoncadaCL <lucasmoncada017@gmail.com>
2025-06-23 01:44:59 -0400

    Primer commit

---
+++ src/main/java/com/clinicanuevomilenio/usuarios/UsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.clinicanuevomilenio.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosApplication.class, args);
+	}
+
+}
